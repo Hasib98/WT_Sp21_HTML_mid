794130685a9ce92880204326c5383148282db356
Hasib98 <phoenixoffire1998@gmail.com>
2021-02-09 12:49:44 +0600

    5th update

--- LAB3-2/problem_1.php
+++ LAB3-2/problem_1.php
@@ -1,10 +1,17 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
+<html>
   <head>
-    <meta charset="utf-8">
-    <title> </title>
+    <title>this</title>
   </head>
-  <body>
+  <body bgcolor='grey'>
+    <?php
+      $color = array('white', 'green', 'red', 'blue', 'black');
+
+    ?>
+
+
+
+
+    <p>The memory of that scene for me is like a frame of film forever frozen at that moment: the <span style="color:<?php echo  $color[2]; ?>;">red</span> carpet, the <span style="color:<?php echo  $color[1]; ?>;">green</span> lawn, the <span style="color:<?php echo  $color[0]; ?>;">white</span> house, the leaden sky. The new president and his first lady. - Richard M. Nixon</p>
 
   </body>
 </html>
